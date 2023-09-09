3ba2507f04a01b6c572e1c7f263b6f9e24180f17
Hrithik Naha <naha.hrithkk@gmail.com>
2023-09-09 21:49:13 +0530

    add router and outlet with Home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-    return <div>Monied</div>;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
