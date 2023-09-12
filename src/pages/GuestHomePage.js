96165d3f92156c7815c04fdde49f1bba65416deb
Hrithik Naha <naha.hrithkk@gmail.com>
2023-09-12 21:39:34 +0530

    Add button template

--- src/pages/GuestHomePage.js
+++ src/pages/GuestHomePage.js
@@ -3,6 +3,7 @@ const GuestHomePage = () => {
         <div className="min-h-screen  bg-gray-100">
             <div className="mx-auto container px-4">
                 <div className="py-4">Get your expense in order!</div>
+                <button>Magic!</button>
             </div>
         </div>
     );
