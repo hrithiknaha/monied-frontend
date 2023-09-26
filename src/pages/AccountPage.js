2d15a05c359c4c797e7fe572695704cd0cbb331c
Hrithik Naha <naha.hrithkk@gmail.com>
2023-09-26 21:58:45 +0530

    moved tables out of hero content

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -53,92 +53,91 @@ const AccountPage = () => {
                 {isLoading ? (
                     <p>Loading...</p>
                 ) : (
-                    <div className="bg-white shadow-md rounded-lg p-4">
-                        <div className="flex flex-col">
-                            <p className="text-xs text-gray-500">{account.type}</p>
-                            <div className="flex items-center justify-between gap-2 w-full">
-                                <h3 className="text-2xl font-semibold">{account.name}</h3>
-                                <p className="text-xs text-white bg-purple-500 px-2 py-2 rounded-lg">
-                                    {account.entity_name}
-                                </p>
+                    <div className="">
+                        <div className="bg-white shadow-md rounded-lg p-4">
+                            <div className="flex flex-col">
+                                <p className="text-xs text-gray-500">{account.type}</p>
+                                <div className="flex items-center justify-between gap-2 w-full">
+                                    <h3 className="text-2xl font-semibold">{account.name}</h3>
+                                    <p className="text-xs text-white bg-purple-500 px-2 py-2 rounded-lg">
+                                        {account.entity_name}
+                                    </p>
+                                </div>
+
+                                {account.type === "BANK" ? (
+                                    <div>
+                                        <p className="pt-4 text-4xl">₹{account.balance}</p>
+                                        <div className="flex justify-between items-center my-4">
+                                            <button
+                                                onClick={() => setopenAccountIncomeModal(true)}
+                                                className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-lg mr-4">
+                                                Add Income
+                                            </button>
+                                            <button
+                                                onClick={() => setopenAccountExpenseModal(true)}
+                                                className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg">
+                                                Add Expense
+                                            </button>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <p className="pt-4 text-4xl">₹{account.amount_due}</p>
+                                        <div className="flex justify-between items-center mt-4">
+                                            <button
+                                                onClick={() => setopenAccountRepaymentModal(true)}
+                                                className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-lg mr-4">
+                                                Add Repayment
+                                            </button>
+                                            <button
+                                                onClick={() => setopenAccountExpenseModal(true)}
+                                                className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg">
+                                                Add Expense
+                                            </button>
+                                        </div>
+                                    </div>
+                                )}
                             </div>
 
-                            {account.type === "BANK" ? (
-                                <div>
-                                    <p className="pt-4 text-4xl">₹{account.balance}</p>
-                                    <div className="flex justify-center items-center my-4">
-                                        <button
-                                            onClick={() => setopenAccountIncomeModal(true)}
-                                            className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-lg mr-4">
-                                            Add Income
-                                        </button>
-                                        <button
-                                            onClick={() => setopenAccountExpenseModal(true)}
-                                            className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg">
-                                            Add Expense
-                                        </button>
-                                    </div>
-                                </div>
-                            ) : (
-                                <div>
-                                    <p className="pt-4 text-4xl">₹{account.amount_due}</p>
-                                    <div className="flex justify-center items-center my-4">
-                                        <button
-                                            onClick={() => setopenAccountRepaymentModal(true)}
-                                            className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-lg mr-4">
-                                            Add Repayment
-                                        </button>
-                                        <button
-                                            onClick={() => setopenAccountExpenseModal(true)}
-                                            className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg">
-                                            Add Expense
-                                        </button>
-                                    </div>
-                                </div>
+                            {openAccountIncomeModal && (
+                                <AccountIncomeModal
+                                    closeModal={setopenAccountIncomeModal}
+                                    accountId={accountId}
+                                    axiosPrivateInstance={axiosPrivateInstance}
+                                    auth={auth}
+                                />
+                            )}
+                            {openAccountExpenseModal && (
+                                <AccountExpenseModal
+                                    closeModal={setopenAccountExpenseModal}
+                                    categoriesMonth={categoriesMonth}
+                                    accountId={accountId}
+                                    axiosPrivateInstance={axiosPrivateInstance}
+                                    auth={auth}
+                                />
+                            )}
+                            {openAccountRepaymentModal && (
+                                <AccountRepaymentModal
+                                    closeModal={setopenAccountRepaymentModal}
+                                    accounts={allAccounts}
+                                    accountId={accountId}
+                                    axiosPrivateInstance={axiosPrivateInstance}
+                                    auth={auth}
+                                />
                             )}
                         </div>
-
-                        {openAccountIncomeModal && (
-                            <AccountIncomeModal
-                                closeModal={setopenAccountIncomeModal}
-                                accountId={accountId}
-                                axiosPrivateInstance={axiosPrivateInstance}
-                                auth={auth}
-                            />
-                        )}
-                        {openAccountExpenseModal && (
-                            <AccountExpenseModal
-                                closeModal={setopenAccountExpenseModal}
-                                categoriesMonth={categoriesMonth}
-                                accountId={accountId}
-                                axiosPrivateInstance={axiosPrivateInstance}
-                                auth={auth}
-                            />
-                        )}
-                        {openAccountRepaymentModal && (
-                            <AccountRepaymentModal
-                                closeModal={setopenAccountRepaymentModal}
-                                accounts={allAccounts}
-                                accountId={accountId}
-                                axiosPrivateInstance={axiosPrivateInstance}
-                                auth={auth}
-                            />
-                        )}
-
-                        <div className="mt-2">
-                            <h2 className="text-2xl font-semibold bg-green-500 text-white py-2 pl-4">All Expenses</h2>
+                        <div className="mt-6">
+                            <h2 className="text-xl font-semibold bg-green-500 text-white py-2 pl-4">All Expenses</h2>
                             <ExpenseTable expenses={account.expenses} />
                         </div>
                         {account.type === "BANK" ? (
                             <div className="mt-8">
-                                <h2 className="text-2xl font-semibold bg-green-500 text-white py-2 pl-4">
-                                    All Incomes
-                                </h2>
+                                <h2 className="text-xl font-semibold bg-green-500 text-white py-2 pl-4">All Incomes</h2>
                                 <IncomeTable incomes={account.incomes} />
                             </div>
                         ) : (
                             <div className="mt-8">
-                                <h2 className="text-2xl font-semibold bg-green-500 text-white py-2 pl-4">
+                                <h2 className="text-xl font-semibold bg-green-500 text-white py-2 pl-4">
                                     All Repayments
                                 </h2>
                                 <RepaymentTable repayments={account.repayments} />
