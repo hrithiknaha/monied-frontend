9f1b3923c951a02fb412ebf0829a0289aff234e7
Hrithik Naha <naha.hrithkk@gmail.com>
2023-10-02 01:39:34 +0530

    filter logic to expense and repayments

--- src/pages/ExpensesPage.js
+++ src/pages/ExpensesPage.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import moment from "moment";
 import { useSelector } from "react-redux";
 
 import { getUserAuth } from "../redux/features/auth/authSlice";
@@ -13,14 +14,61 @@ const ExpensesPage = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [expenses, setExpenses] = useState([]);
 
+    const [filteredExpenses, setFilteredExpenses] = useState([]);
+
+    const [expenseYears, setExpenseYears] = useState([]);
+    const [expenseMonths, setExpenseMonths] = useState([]);
+    const [selectedExpenseYear, setSelectedExpenseYear] = useState(moment(Date.now()).utc().year());
+    const [selectedExpenseMonth, setSelectedExpenseMonth] = useState(moment(Date.now()).utc().format("MMMM"));
+
     useEffect(() => {
         const axiosInstance = axiosPrivateInstance(auth.token);
 
         axiosInstance.get("/api/expenses").then(({ data }) => {
             setExpenses(data.data);
             setIsLoading(false);
+
+            const expenseYears = data.data.map((expense) => {
+                return moment(expense.transaction_date).utc().year();
+            });
+
+            const expenseMonths = data.data.map((expense) => {
+                return {
+                    monthName: moment(expense.transaction_date).utc().format("MMMM"),
+                    monthNumber: moment(expense.transaction_date).utc().month(),
+                };
+            });
+
+            setExpenseYears([...new Set(expenseYears.reverse())]);
+
+            expenseMonths.sort((a, b) => a.monthNumber - b.monthNumber);
+            const sortedMonthNames = expenseMonths.map((month) => month.monthName);
+            setExpenseMonths([...new Set(sortedMonthNames)]);
         });
     }, []);
+
+    useEffect(() => {
+        const filterExpense = () => {
+            if (selectedExpenseMonth === "all" && selectedExpenseYear === "all") {
+                setFilteredExpenses(expenses);
+            } else {
+                const filteredData = expenses.filter((item) => {
+                    const itemDate = moment(item.transaction_date);
+                    const itemMonth = itemDate.format("MMMM");
+                    const itemYear = itemDate.year();
+
+                    return (
+                        (selectedExpenseMonth === "all" || itemMonth === selectedExpenseMonth) &&
+                        (selectedExpenseYear === "all" || itemYear === parseInt(selectedExpenseYear))
+                    );
+                });
+
+                setFilteredExpenses(filteredData);
+            }
+        };
+
+        filterExpense();
+    }, [expenses, selectedExpenseMonth, selectedExpenseYear]);
 
     return (
         <div className="min-h-screen bg-gray-100">
@@ -30,7 +78,51 @@ const ExpensesPage = () => {
                 ) : (
                     <div className="mt-6">
                         <h2 className="text-gray-600 text-xl font-semibold text-center">All Expenses</h2>
-                        <ExpenseTable expenses={expenses} />
+                        <div className="flex gap-4 justify-center mt-8">
+                            <div className="flex gap-2 items-center mb-4">
+                                <label htmlFor="expenseYears" className="block text-gray-700 font-bold">
+                                    Year
+                                </label>
+                                <select
+                                    id="expenseYears"
+                                    name="expenseYears"
+                                    value={selectedExpenseYear}
+                                    onChange={(e) => setSelectedExpenseYear(e.target.value)}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    required>
+                                    <option value="all">All</option>
+                                    {expenseYears.map((year) => {
+                                        return (
+                                            <option key={year} value={year}>
+                                                {year}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                            <div className="flex gap-2 items-center mb-4">
+                                <label htmlFor="expenseMonths" className="block text-gray-700 font-bold">
+                                    Month
+                                </label>
+                                <select
+                                    id="expenseMonths"
+                                    name="expenseMonths"
+                                    value={selectedExpenseMonth}
+                                    onChange={(e) => setSelectedExpenseMonth(e.target.value)}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    required>
+                                    <option value="all">All</option>
+                                    {expenseMonths.map((month) => {
+                                        return (
+                                            <option key={month} value={month}>
+                                                {month}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                        </div>
+                        <ExpenseTable expenses={filteredExpenses} />
                     </div>
                 )}
             </div>
--- src/pages/RepaymentsPage.js
+++ src/pages/RepaymentsPage.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import moment from "moment";
 import { useSelector } from "react-redux";
 
 import { getUserAuth } from "../redux/features/auth/authSlice";
@@ -13,14 +14,61 @@ const RepaymentsPage = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [repayments, setRepayments] = useState([]);
 
+    const [filteredRepayments, setFilteredRepayments] = useState([]);
+
+    const [repaymentYears, setRepaymentYears] = useState([]);
+    const [repaymentMonths, setRepaymentMonths] = useState([]);
+    const [selectedRepaymentYear, setSelectedRepaymentYear] = useState(moment(Date.now()).utc().year());
+    const [selectedRepaymentMonth, setSelectedRepaymentMonth] = useState(moment(Date.now()).utc().format("MMMM"));
+
     useEffect(() => {
         const axiosInstance = axiosPrivateInstance(auth.token);
 
         axiosInstance.get("/api/repayments").then(({ data }) => {
             setRepayments(data.data);
             setIsLoading(false);
+
+            const repaymentYears = data.data.map((repayment) => {
+                return moment(repayment.transaction_date).utc().year();
+            });
+
+            const repaymentMonths = data.data.map((repayment) => {
+                return {
+                    monthName: moment(repayment.transaction_date).utc().format("MMMM"),
+                    monthNumber: moment(repayment.transaction_date).utc().month(),
+                };
+            });
+
+            setRepaymentYears([...new Set(repaymentYears.reverse())]);
+
+            repaymentMonths.sort((a, b) => a.monthNumber - b.monthNumber);
+            const sortedMonthNames = repaymentMonths.map((month) => month.monthName);
+            setRepaymentMonths([...new Set(sortedMonthNames)]);
         });
     }, []);
+
+    useEffect(() => {
+        const filterRepayment = () => {
+            if (selectedRepaymentMonth === "all" && selectedRepaymentYear === "all") {
+                setFilteredRepayments(repayments);
+            } else {
+                const filteredData = repayments.filter((item) => {
+                    const itemDate = moment(item.transaction_date);
+                    const itemMonth = itemDate.format("MMMM");
+                    const itemYear = itemDate.year();
+
+                    return (
+                        (selectedRepaymentMonth === "all" || itemMonth === selectedRepaymentMonth) &&
+                        (selectedRepaymentYear === "all" || itemYear === parseInt(selectedRepaymentYear))
+                    );
+                });
+
+                setFilteredRepayments(filteredData);
+            }
+        };
+
+        filterRepayment();
+    }, [repayments, selectedRepaymentMonth, selectedRepaymentYear]);
 
     return (
         <div className="min-h-screen bg-gray-100">
@@ -30,7 +78,51 @@ const RepaymentsPage = () => {
                 ) : (
                     <div className="mt-6">
                         <h2 className="text-gray-600 text-xl font-semibold text-center">All Repayments</h2>
-                        <RepaymentTable repayments={repayments} />
+                        <div className="flex gap-4 justify-center mt-8">
+                            <div className="flex gap-2 items-center mb-4">
+                                <label htmlFor="repaymentYears" className="block text-gray-700 font-bold">
+                                    Year
+                                </label>
+                                <select
+                                    id="repaymentYears"
+                                    name="repaymentYears"
+                                    value={selectedRepaymentYear}
+                                    onChange={(e) => setSelectedRepaymentYear(e.target.value)}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    required>
+                                    <option value="all">All</option>
+                                    {repaymentYears.map((year) => {
+                                        return (
+                                            <option key={year} value={year}>
+                                                {year}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                            <div className="flex gap-2 items-center mb-4">
+                                <label htmlFor="repaymentMonths" className="block text-gray-700 font-bold">
+                                    Month
+                                </label>
+                                <select
+                                    id="repaymentMonths"
+                                    name="repaymentMonths"
+                                    value={selectedRepaymentMonth}
+                                    onChange={(e) => setSelectedRepaymentMonth(e.target.value)}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    required>
+                                    <option value="all">All</option>
+                                    {repaymentMonths.map((month) => {
+                                        return (
+                                            <option key={month} value={month}>
+                                                {month}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                        </div>
+                        <RepaymentTable repayments={filteredRepayments} />
                     </div>
                 )}
             </div>
